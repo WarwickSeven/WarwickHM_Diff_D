6d3bb2019e02f3ca04ff29e6f8c346b7b20b917b
Warwick <WarwickSeven@gmail.com>
2022-06-15 16:07:37 +0300

    Lesson D Task 1

--- WarwickHM_Diff_D/main.cpp
+++ WarwickHM_Diff_D/main.cpp
@@ -3,29 +3,24 @@
 #include <algorithm>
 
 template <typename T> void printContainer(const T &cnt) {
-    std::cout << "Capacity: " << cnt.capacity() << std::endl;
     for (auto i : cnt) {
          std::cout << i << " ";
      }
-     std::cout << std::endl;
+    std::cout << '\n' << "Capacity: " << cnt.capacity() << std::endl;;
  }
 
 template <typename T>
 void insert_sorted(std::vector<T> &v, const T &i) {
-    sort(v.begin(), v.end());
-    std::vector<int>::iterator low;
-    low = lower_bound(v.begin(), v.end(), i);
-    std::cout << "insert new data to position: " << (low - v.begin()) << std::endl;
-    v.insert(low, i);
+    v.insert(lower_bound(v.begin(), v.end(), i), i);
     v.shrink_to_fit();
 }
 
 int main() {
-    using MyType = int;
+    using MyType = double;
     std::vector<MyType> MyVec = {10, 20, 30, 40, 50, 60, 70};
-    MyType MyInt = 35;
+    MyType MyNum = 35.5;
     printContainer(MyVec);
-    insert_sorted(MyVec, MyInt);
+    insert_sorted(MyVec, MyNum);
     printContainer(MyVec);
     return 0;
 }
